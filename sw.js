5fd70509c0106fbb79a6c3ce85c22b0fd3058309
LivesCreate <252925942+LivesCreate@users.noreply.github.com>
2026-04-26 09:24:51 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'flux-v5-4-2';
+var CACHE_NAME = 'flux-v5-4-3';
 var URLS_TO_CACHE = ['./', './index.html'];
 self.addEventListener('install', function(e) { e.waitUntil(caches.open(CACHE_NAME).then(function(c) { return c.addAll(URLS_TO_CACHE); }).then(function() { return self.skipWaiting(); })); });
 self.addEventListener('activate', function(e) { e.waitUntil(caches.keys().then(function(n) { return Promise.all(n.filter(function(k) { return k !== CACHE_NAME; }).map(function(k) { return caches.delete(k); })); }).then(function() { return self.clients.claim(); })); });
